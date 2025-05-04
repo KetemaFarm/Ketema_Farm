db6a74d084d9c05c811f5e1365bd0baf00ffcd1d
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2025-05-04 18:07:54 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -74,13 +74,27 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database Configuration (PostgreSQL for production)
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-        ssl_require=True
-    )
-}
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         default=os.getenv('DATABASE_URL'),
+#         conn_max_age=600,
+#         ssl_require=True
+#     )
+# }
+import dj_database_url
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
