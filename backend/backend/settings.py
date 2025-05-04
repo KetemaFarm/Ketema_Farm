cabab281914fec7675cb9ce13abb17a670ca5803
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2025-05-04 18:39:12 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -81,19 +81,22 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 #         ssl_require=True
 #     )
 # }
-import dj_database_url
-
-if 'DATABASE_URL' in os.environ:
-    DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE', 'ketema_farm'),
+        'USER': os.getenv('PGUSER', 'ketema_farm_user'),
+        'PASSWORD': os.getenv('PGPASSWORD', 'dJ7FGYe5x16Ege5ViSa2mvXbZdy1LmMf'),
+        'HOST': os.getenv('PGHOST', 'dpg-d0bhqn2dbo4c73coi0i0-a'),
+        'PORT': os.getenv('PGPORT', '5432'),
+        'OPTIONS': {
+            'sslmode': 'require',
+            'sslrootcert': os.path.join(BASE_DIR, 'prod-ca-2021.crt'),
+            'connect_timeout': 5,  # Prevents hanging during connection issues
+        },
+        'CONN_MAX_AGE': 600,  # Connection persistence (in seconds)
     }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+}
 
 
 # Password validation
