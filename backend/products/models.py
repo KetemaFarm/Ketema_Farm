a0d6ce37ec2d4c06162bbb8f837935f5c23a628e
Yared <149290215+yared-Habtamu@users.noreply.github.com>
2025-05-05 17:47:04 +0300

    Update models.py

--- backend/products/models.py
+++ backend/products/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from users.models import User
-# from cloudinary_storage.storage import MediaCloudinaryStorage
+from cloudinary_storage.storage import MediaCloudinaryStorage
+
 
 class Product(models.Model):
     CATEGORIES = [
@@ -29,7 +30,12 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     category = models.CharField(max_length=20, choices=CATEGORIES)
     quantity = models.PositiveIntegerField(default=1)
-    image = models.ImageField(upload_to='products/', blank=True, null=True)
+    image = models.ImageField(
+        upload_to='products/',
+        storage=MediaCloudinaryStorage(),  # Explicit storage backend
+        blank=True,
+        null=True
+    )
     created_at = models.DateTimeField(auto_now_add=True)
     city = models.CharField(max_length=20, choices=CITY_CHOICES)
 
